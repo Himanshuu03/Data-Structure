5e1d782e6753c08b9b3f6092d2108ed9ca327c43
Himanshuu03 <himanshujangra2403@gmail.com>
2023-09-11 19:44:04 +0530

    Number System - binary to decimal

--- NumberSystem/dectoBinary.cpp
+++ NumberSystem/dectoBinary.cpp
@@ -15,9 +15,30 @@ int decToBinary(int n){
     return ans;
 }
 
+
+//by bitwise method
+
+int decimalToBinary(int n){
+    int ans = 0;
+    int i =0;
+    while (n!=0)
+    {
+        int bit = n&1;
+        ans = bit*pow(10,i++)+ans;
+        n=n>>1;
+    }
+    
+    return ans;
+}
+
 int main(){
+
     int n;
     cin>>n;
-    cout<<decToBinary(n);
+    //division method
+    // cout<<decToBinary(n);
+
+    //bitwise method
+    cout<<decimalToBinary(n);
 return 0;
 }
