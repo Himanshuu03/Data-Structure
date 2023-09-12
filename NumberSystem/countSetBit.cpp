39e5345c72b64f232da477a1ef583196c3a53841
Himanshuu03 <himanshujangra2403@gmail.com>
2023-09-12 20:03:58 +0530

    Number-System

---
+++ NumberSystem/countSetBit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int countBit(int num){
+    int count =0;
+    while(num != 0){
+        int bit = num & 1;
+        if(bit == 1){
+            count ++;
+        }
+        num = num>>1;
+    }
+    return count;
+}
+int main(){
+    int num;
+    cin>>num;
+    cout<<countBit(num);
+return 0;
+}
