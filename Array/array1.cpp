3b1b332a98e0450f47fbbf92463a2be1480e2fae
Himanshuu03 <himanshujangra2403@gmail.com>
2023-09-13 19:39:01 +0530

    count ones and zero

--- Array/array1.cpp
+++ Array/array1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<limits.h>
 using namespace std;
 
 bool linearSearch(int arr[],int num,int find){
@@ -11,6 +12,30 @@ bool linearSearch(int arr[],int num,int find){
     return false;
 }
 
+void countZeroOnes(int arr[],int num){
+    int ones=0,zeros =0;
+    for(int i=0;i<num;i++){
+        if(arr[i] == 1){
+            ones++;
+        }
+        if(arr[i] == 0){
+            zeros++;
+        }
+    }
+    cout<<"The zero are"<<zeros<<endl;
+    cout<<"The ones are"<<ones<<endl;
+}
+
+int findInTheArray(int arr[] ,int num){
+    int max = INT_MAX;
+    for(int i =0;i<num;i++){
+        if(arr[i]<max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
 int main(){
     int num;
     cin>>num;
@@ -18,8 +43,14 @@ int main(){
     for(int i=0;i<num;i++){
         cin >> arr[i];
     }
+
+
+    //Question 1
     // cout<<"Enter the num you want to find"<<endl;
     // int find;
     // cin>>find;
     // cout<<linearSearch(arr,num,find);
+
+   //Question 2
+   // countZeroOnes(arr,num);
 }
