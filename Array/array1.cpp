04b1bf4fd963b02797eba379523a8b65e19732c7
Himanshuu03 <himanshujangra2403@gmail.com>
2023-09-13 19:31:57 +0530

    linear-search

--- Array/array1.cpp
+++ Array/array1.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
-int main(){
 
-return 0;
+bool linearSearch(int arr[],int num,int find){
+    bool check = false;
+    for(int i=0;i<num;i++){
+        if(arr[i] == find){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    int num;
+    cin>>num;
+    int arr[num];
+    for(int i=0;i<num;i++){
+        cin >> arr[i];
+    }
+    // cout<<"Enter the num you want to find"<<endl;
+    // int find;
+    // cin>>find;
+    // cout<<linearSearch(arr,num,find);
 }
