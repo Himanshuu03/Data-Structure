5cc22e6978208bb7b7332a5e683750ae97e72e37
Himanshuu03 <himanshujangra2403@gmail.com>
2023-09-16 12:32:47 +0530

    array

---
+++ Array/arrayQuestion.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<math.h>
+#include<vector>
+using namespace std;
+int main(){
+    // int arr[] ={1,2,-3,4,-5,6};
+    // int left = 0;
+    // int right = 5;
+    // while(left<=right){
+    //     if(arr[left] < 0){
+    //         left++;
+    //     }
+    //     else if(arr[right] >0){
+    //         right--;
+    //     }
+    //     else{
+    //         swap(arr[left++],arr[right--]);
+    //     }
+    // }
+    // for(int i=0;i<6;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+
+    int arr[] = {1,3,5,3,4};
+    int size = sizeof(arr) /sizeof(int);
+    for(int i =0;i<size;i++){
+        int index = abs(arr[i]);
+        if(arr[index-1] > 0){
+            arr[index-1] *= -1;
+        }
+    }
+    for(int i=0;i<size;i++){
+        if(arr[i]>0){
+          cout<<i+1;
+        }
+    }
+return 0;
+}
